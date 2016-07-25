b3ca309082758d24b1e2136702a621806b24ee5a
jiyi <jiyi1023@foxmail.com>
2016-07-25 22:07:05 +0800

    完善初审结束后学生显示的状态

--- resources/views/index/review.blade.php
+++ resources/views/index/review.blade.php
@@ -10,7 +10,10 @@
                     <div class="panel-body">
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-md-offset-3 text-center">
-                                @if($register)
+                                @if(count($register)==0)
+                                    <h2 class="fast"><i class="fa fa-stop-circle-o"></i> 对不起，您来晚了 </h2>
+                                    <p>苏州科技大学敬文新教育书院报名已经截止啦~~</p>
+                                @elseif($register->review_state == 2)
                                     <h2 class="fast"><i class="fa fa-smile-o"></i> 恭喜您已通过初审 </h2>
                                     <p>接下来会进行复审，请耐心等到录取通知结果！</p>
                                 @else
