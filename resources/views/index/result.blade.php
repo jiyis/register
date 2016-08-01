1bd70764bb5107ce671555648c9565e1c11a16dc
Gary P Dong <Gary.P.Dong@BenQ.com>
2016-08-01 14:33:46 +0800

    报名成功后的显示信息

--- resources/views/index/result.blade.php
+++ resources/views/index/result.blade.php
@@ -12,7 +12,7 @@
                             <div class="col-lg-6 col-md-6 col-md-offset-3 text-center">
                                 <h2 class="fast"><i class="fa fa-clock-o"></i> 您已经成功报名 </h2>
                                 <ol class="text-left">
-                                    <li>请继续将视频资料上传到邮箱:jw_newedu@126.com,视频格式为mp4、mkv.</li>
+                                    <li>请继续将视频资料上传到邮箱:jw_newedu@126.com,视频格式为mp4、mkv、rm、rmvb、avi、wmv.</li>
                                     <li>8月20日10:00再次登录该报名系统，查询初审结果。</li>
                                 </ol>
                             </div>
